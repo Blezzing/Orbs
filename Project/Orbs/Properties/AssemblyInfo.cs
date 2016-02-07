6eefeac81eb63a5ad0ef4b3f50b551ce5054659a
Blezzing <mmhmaster@hotmail.com>
2016-02-07 16:39:46 +0100

    Assemblies moved to seperate folder.

--- Project/Orbs/Properties/AssemblyInfo.cs
+++ Project/Orbs/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Orbs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Blezzing")]
 [assembly: AssemblyProduct("Orbs")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
